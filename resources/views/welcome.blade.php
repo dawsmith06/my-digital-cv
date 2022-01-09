c06d4c76e3960b725b6f62d98a1ef63008aa8485
Darwin Smith <darwrosso@gmail.com>
2022-01-08 23:47:56 -0400

    title color depending on prefers-color-scheme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,6 +28,17 @@
                 opacity: 0.5;
                 pointer-events:none;
             }
+            @media (prefers-color-scheme: light) {
+                .title   {
+                    color: rgb(69, 64, 65); 
+                }
+            }
+            @media (prefers-color-scheme: dark) {
+                .title   {
+                    color: white; 
+                }
+            }
+
         </style>
     </head>
     <body class="antialiased">
@@ -48,7 +59,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                  <h1 class="text-center" style="width:100%; color: white;">Digital Curriculum Vitae</h1>
+                  <h1 class="text-center title" style="width:100%;">Digital Curriculum Vitae</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
